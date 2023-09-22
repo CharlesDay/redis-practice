905c066875e9626ff288bfebb9d1db3b9fd285d0
Charles Day <cdayjr1997@gmail.com>
2023-09-22 00:55:59 -0400

    fix minor mistake

--- src/main/java/com/charlie/redis/DisableOnWindows.java
+++ src/main/java/com/charlie/redis/DisableOnWindows.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-@ExtendWith(DisableOnMacCondition.class)
+@ExtendWith(DisableOnWindowsCondition.class)
 public @interface DisableOnWindows {
 }
